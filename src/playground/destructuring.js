3cd9c362ac91fdc8f3f41cc63ccfddd192258a99
Amrit Singh <amrit.sbagga@gmail.com>
2021-02-14 01:40:48 +0530

    Array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,38 +1,58 @@
-const person = {
-    name : "Amrit",
-    age : 31,
-    location : {
-        city : "Sarangarh",
-        temp : 21
-    }
-};
+// Object destructuring
 
-//destructuring
-const { name : firstName = 'Anonymous', age } = person;
-const { temp : temperature, city } = person.location;
+// const person = {
+//     name : "Amrit",
+//     age : 31,
+//     location : {
+//         city : "Sarangarh",
+//         temp : 21
+//     }
+// };
 
-//const name = person.name;
-//const age = person.age;
+// //destructuring
+// const { name : firstName = 'Anonymous', age } = person;
+// const { temp : temperature, city } = person.location;
 
-console.log(`${firstName} is ${age}`);
+// //const name = person.name;
+// //const age = person.age;
 
-if(city && temperature){
-    console.log(`Its ${temperature} in ${city}`);
-}
+// console.log(`${firstName} is ${age}`);
 
-
-const book = {
-    title : "Ego is the Enemy",
-    author : "Ryan Holiday",
-    publisher : {
-        name : 'Penguin'
-    }
-}
-
-const { publisher } = book;
-const { name : publisherName = "Self-published" } = publisher;
-
-console.log(publisherName);
+// if(city && temperature){
+//     console.log(`Its ${temperature} in ${city}`);
+// }
 
 
-//console.log("destructuring..!!");
+// const book = {
+//     title : "Ego is the Enemy",
+//     author : "Ryan Holiday",
+//     publisher : {
+//         name : 'Penguin'
+//     }
+// }
+
+// const { publisher } = book;
+// const { name : publisherName = "Self-published" } = publisher;
+
+// console.log(publisherName);
+
+
+// //console.log("destructuring..!!");
+
+
+// Array destructuring
+
+const address = ['1299 S Juniper Street', 'New York', 'US', '19144'];
+
+const [street, city, state="Michigan"] = address;
+//const [, city, state] = address;
+
+console.log(`You are in ${street}`); //address[0]
+console.log(`You are in ${city}`); //address[1]
+console.log(`You are in ${state}`); //address[2]
+
+const items = ["Coffee (hot)", "$2.00", "$2.50", "$2.75"];
+// Array destructuring for items
+const [itemName = "Coffee(cold)", , mediumPrice] = items;
+
+console.log(`Medium ${itemName} price has cost of : ${mediumPrice}`);
