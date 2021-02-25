6a9d1c545a1ab489266c5f380adf5a1f609151f8
Amrit Singh <amrit.sbagga@gmail.com>
2021-02-25 20:49:40 +0530

    jest case added

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,5 +1,11 @@
-const add = (a, b) => a + b;
+const add = (a, b) => a + b + 1;
 
 test('should add two numbers', () => {
     const result = add(3, 4);
+
+    expect(result).toBe(7);
+
+    // if(result !== 7){
+    //     throw new Error(`You added 4 & 3. The result was ${result}. Expected 7`);
+    // }
 });
