7f19b384ff686557b303de9e09356757a05abf0f
Amrit Singh <amrit.sbagga@gmail.com>
2021-03-02 20:45:02 +0530

    jest case added for start date

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import selectExpenses from '../../selectors/expenses';
 
 const expenses = [{
@@ -11,13 +12,13 @@ const expenses = [{
     description : "Rent",
     note : "",
     amount : 1095,
-    createdAt : -1000
+    createdAt : moment(0).subtract(4, 'days').valueOf()
 }, {
     id : 3,
     description : "Credit card",
     note : "",
     amount : 4500,
-    createdAt : 1000
+    createdAt : moment(0).add(4, 'days').valueOf()
 }]
 
 test('should filter by test value', () => {
@@ -29,4 +30,26 @@ test('should filter by test value', () => {
     };
     const result = selectExpenses();
     expect(result).toEqual([ expenses[2], expenses[1]]);
-})
+});
+
+test('should filter by startDate', () => {
+    const filters = {
+       text : '',
+       sortBy : 'date',
+       startDate : moment(0),
+       endDate: undefined 
+    };
+    const result = selectExpenses(expenses, filters);
+    expect(result).toEqual([ expenses[2], expenses[0]]);
+});
+
+//should filter by endDate
+
+
+
+//should sort by date
+
+
+
+//should filter by amount
+
