9be12c00335e56fa88679c244bbe4ce67552bf56
Amrit Singh <amrit.sbagga@gmail.com>
2021-03-05 20:20:02 +0530

    jest remaining filter reducer test case added

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -9,4 +9,48 @@ test('should setup default filter values', () => {
         startDate : moment().startOf('month'),
         endDate : moment().endOf('month')
     });
-})
+});
+
+test('should setup sortBy to amount', () => {
+    const state = filtersReducer(undefined, { type : 'SORT_BY_AMOUNT'});
+    expect(state.sortBy).toBe('amount');
+});
+
+test('should setup sortBy to date', () => {
+    const currentState = {
+        text : '',
+        startDate : undefined,
+        endDate : undefined,
+        sortBy : 'amount'
+    };
+
+    const action = {
+        type : 'SORT_BY_DATE'
+    };
+    const state = filtersReducer(currentState, action);
+    expect(state.sortBy).toBe('date');
+});
+
+test('should set text filter', () => {
+    const text = 'This is my filter';
+    const action = {
+        type : 'SET_TEXT_FILTER',
+        text
+    }
+    const state = filtersReducer(undefined, action);
+    expect(state.text).toBe(text);
+});
+
+test('should set startDate filter', () => {
+    const startDate = moment();
+    const action = {
+        type : 'SET_START_DATE',
+        startDate
+    };
+    const state = filtersReducer(undefined, action);
+    expect(state.startDate).toEqual(startDate);
+});
+
+test('should set endDate filter', () => {
+
+});
