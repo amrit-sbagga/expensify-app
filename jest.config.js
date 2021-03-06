e6fc7b7ee8792cfa6a6953e0e98b14297411e759
Amrit Singh <amrit.sbagga@gmail.com>
2021-03-06 14:34:12 +0530

    babel & jest config changes

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
+    //preset: 'preset-env',
+    verbose : true,
     transform: {},
-};
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    transformIgnorePatterns: [
+      "node_modules/(?!(moment"
+        // + "|react-navigation-tabs"
+        // + "|react-native-splash-screen"
+        // + "|react-native-screens"
+        // + "|react-native-reanimated"
+      + ")/)",
+    ],
+  }
