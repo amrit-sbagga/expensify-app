457013ff4be841d3e562dd19dd1333d4f5a6053e
Amrit Singh <amrit.sbagga@gmail.com>
2021-03-22 20:20:16 +0530

    production webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -3,40 +3,46 @@
 const path = require('path')
 //console.log(path.join(__dirname, 'public'));
 
-module.exports = {
-    entry :  [
-        'react-dates/initialize',
-        'react-dates/lib/css/_datepicker.css',
-        './src/app.js'
-      ],
-   // entry : './src/app.js',
-    output : {
-        path: path.join(__dirname, 'public'),
-        filename : 'bundle.js'
-    },
-    module: {
-        rules : [
-            {
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            },
-            {
-                test : /\.s?css$/,
-                use :[
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    devtool : 'cheap-module-source-map',
-    mode:'development',
-    devServer : {
-        contentBase : path.join(__dirname, 'public'),
-        historyApiFallback : true
-    }
+module.exports = (env) => {
+
+    const isProduction = env === 'production';
+
+    console.log('env = ', env);
+    return {
+        entry :  [
+            'react-dates/initialize',
+            'react-dates/lib/css/_datepicker.css',
+            './src/app.js'
+        ],
+        // entry : './src/app.js',
+        output : {
+            path: path.join(__dirname, 'public'),
+            filename : 'bundle.js'
+        },
+        module: {
+            rules : [
+                {
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/
+                },
+                {
+                    test : /\.s?css$/,
+                    use :[
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                }
+            ]
+        },
+        devtool : isProduction? 'source-map' : 'cheap-module-source-map',
+        mode:'development',
+        devServer : {
+            contentBase : path.join(__dirname, 'public'),
+            historyApiFallback : true
+        }
+    };
 };
 
 //check webpack docs available at https://webpack.js.org/configuration 
