353945bad8216b7c8ccc016830ec90c77db131bd
Amrit Singh <amrit.sbagga@gmail.com>
2021-04-05 13:49:56 +0530

    updating data in firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -21,7 +21,12 @@ const firebaseConfig = {
   database.ref().set({
       name : "Amrit Singh",
       age : 31,
+      stressLevel : 6,
       isSingle : true,
+      job : {
+        title : 'Sotware Developer',
+        company : 'Google'
+      },
       location : {
           city :'Sgh',
           country : 'India'
@@ -31,6 +36,23 @@ const firebaseConfig = {
   }).catch((err) => {
       console.log("Error : ", err);
   });
+
+  //update -> only update specified fields
+  // or add new property
+  database.ref().
+      update({
+            name : 'Amr',
+            age : 29,
+            //job : 'Software developer',
+            isSingle : null, //will remove this field
+            'location/city' : 'Mumbai',
+            stressLevel : 7,
+            'job/company': 'Amazon'
+      }).then(() => {
+          console.log('Data is updated!');
+      }).catch((err) => {
+          console.log("Error : ", err);
+      });
 
   //removing data using set using null
  // database.ref('isSingle').set(null);
