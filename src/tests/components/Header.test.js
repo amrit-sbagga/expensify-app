fbc4d2601fc88101d32608184140eb4ae61b5919
Amrit Singh <amrit.sbagga@gmail.com>
2021-04-13 16:47:14 +0530

    google logout test case added

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -3,10 +3,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
 //import ReactShallowRenderer from 'react-test-renderer/shallow';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 
 test('should render Header correctly', () => {
-    const wrapper = shallow(<Header />);
+    const wrapper = shallow(<Header startLogout={() => {
+
+    }}/>);
     expect(toJSON(wrapper)).toMatchSnapshot();
     
     //expect(wrapper.find('h1').text()).toBe('Expensify');
@@ -15,5 +17,14 @@ test('should render Header correctly', () => {
    // renderer.render(<Header />);
     // console.log(renderer.getRenderOutput());
    // expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
+
+
+//should call startLogout on button click
+test('should call startLogout on button click', () => {
+    const startLogout = jest.fn();
+    const wrapper = shallow(<Header startLogout={startLogout} />);
+    wrapper.find('button').simulate('click');
+    expect(startLogout).toHaveBeenCalled();
 });
 
